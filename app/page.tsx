c44d9b8775f3591c3d30b579d02c69a0bf159fb8
Victor Arruda <albq.victor@gmail.com>
2024-11-07 08:45:28 -0300

    chore: add prettier tailwind plugin

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center w-screen">
+      <h1 className="text-2xl">Transactions</h1>
+    </div>
+  )
+}
+
+export default Home;
---
+++ app/transactions/[id]/page.tsx
@@ -0,0 +1,9 @@
+function Transation({params: {id}}) {
+  return (
+    <div>
+      <h1>Transation {id}</h1>
+    </div>
+  );  
+}
+
+export default Transation;
